5b8babe8fd5eddeb718a7979901391894925e028
Andrey Metelkin <andrey.metelkin@stenn.com>
2023-07-17 12:49:19 +0300

    A new test has been created! Added social_networks and elements pages. New classes have been created.

--- fixtures.py
+++ fixtures.py
@@ -1,6 +1,9 @@
+import time
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
 from config import Config
 
 
@@ -12,3 +15,17 @@ def driver():
     driver.get(Config.BASE_URL)
     yield driver
     driver.quit()
+
+
+@pytest.fixture
+def slow_scroll():
+    def scroll(driver, scroll_step=150, pause_duration=0.3):
+        current_position = driver.execute_script('return window.pageYOffset')
+        page_height = driver.execute_script('return document.body.scrollHeight')
+
+        while current_position < page_height:
+            current_position += scroll_step
+            driver.execute_script(f'window.scrollTo(0, {current_position})')
+            time.sleep(pause_duration)
+
+    return scroll
