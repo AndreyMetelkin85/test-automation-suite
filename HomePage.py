bcb40bed819c8634b67b08c3f220438fb6d3fd0f
Andrey Metelkin <andrey.metelkin@stenn.com>
2023-07-18 23:34:21 +0300

    Added a new test!  Added a new class. Minor improvements have been made.

---
+++ HomePage.py
@@ -0,0 +1,56 @@
+from selenium.webdriver.common.by import By
+from elements import Button, Text, Label
+
+
+class HomePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def apply_for_finance_button_1(self) -> Button:
+        return self.driver.find_element(By.XPATH, '//span[contains(.,"Apply for Finance")]')
+
+    def apply_for_finance_button_2(self) -> Button:
+        return self.driver.find_element(By.XPATH, '//div[@id="mainContentContainer"]/div[2]/div/button/span')
+
+    def navi_bar_button(self) -> Button:
+        return self.driver.find_elements(By.XPATH, '//div[@id="navBarWrapper"]/div')
+
+    def right_for_you(self) -> Text:
+        return self.driver.find_element(By.XPATH, '//div[text()="Choose, What’s Right For You?"]')
+
+    def invoice_financing_label(self) -> Text:
+        return self.driver.find_element(By.XPATH, '//h1[text()="International and Domestic Invoice Financing"]')
+
+    def e_commerce_label(self) -> Text:
+        return self.driver.find_element(By.XPATH, '//h1[text()="E-commerce"]')
+
+    def saas_label(self) -> Text:
+        return self.driver.find_element(By.XPATH, '//h1[text()="SaaS"]')
+
+    def convert_your_invoices_label(self) -> Text:
+        return self.driver.find_element(By.XPATH, '//h1[contains(.,"Convert your invoices")]')
+
+    def looking_financing_label(self) -> Text:
+        return self.driver.find_element(By.XPATH,
+                                        '//div[text()="Looking for a financing solution that helps your SaaS business grow?"]')
+
+    def what_stenn_label(self) -> Label:
+        return self.driver.find_elements(By.XPATH, '//div[@id="scrollTarget"]/descendant::h1')
+
+    def what_stenn_button(self) -> Button:
+        return self.driver.find_elements(By.XPATH, '//div[@id="scrollTarget"]/descendant::a')
+
+    def useful_guides_label(self) -> Text:
+        return self.driver.find_element(By.XPATH, '//p[text()="Useful Guides"]')
+
+    def join_stenn_label(self) -> Text:
+        return self.driver.find_element(By.XPATH, '//h1[text()="Join Stenn"]')
+
+    def questions_answers_label(self) -> Text:
+        return self.driver.find_element(By.XPATH, '//h1[text()="Questions & Answers"]')
+
+    def read_news_button(self) -> Button:
+        return self.driver.find_element(By.XPATH, '//span[text()="Read all News"]')
+
+    def apply_online_button(self) -> Button:
+        return self.driver.find_element(By.XPATH, '//span[text()="Apply online"]')
--- UI Tests/testsuitev1.py
+++ UI Tests/testsuitev1.py
@@ -1,5 +1,7 @@
 import time
+from urllib.parse import urlparse
 
+from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -7,6 +9,7 @@ from headerbuttonsverification_page import HeaderButtonsVerification
 from config import highlight
 from fixtures import driver, slow_scroll
 from social_networks import SocialNetworks
+from HomePage import HomePage
 
 
 def test_buttons_in_header(driver):
@@ -50,8 +53,9 @@ def test_buttons_in_header(driver):
 
 def test_opening_social_networks(driver, slow_scroll):
     home_page = HeaderButtonsVerification(driver)
-    pop_up_button = home_page.pop_up_close_button()
-    pop_up_button.click()
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
 
     slow_scroll(driver, scroll_step=100, pause_duration=0.1)
 
@@ -99,8 +103,221 @@ def test_opening_social_networks(driver, slow_scroll):
     youtube_button.social_networks()[3].click()
     time.sleep(3)
 
-    current_window = driver.current_window_handle
     driver.switch_to.window(driver.window_handles[-1])
     current_url = driver.current_url
     expected_url = "https://www.youtube.com/channel/UCVsztAj0QmhKkfu4IKMHUOA"
     assert current_url == expected_url
+
+
+def test_сlick_and_transitions_home_page(driver, slow_scroll):
+    home_page = HeaderButtonsVerification(driver)
+    products_label = home_page.your_working_label()
+    assert products_label.text == "Your working capital platform"
+    highlight(products_label)
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
+
+    page_open = HomePage(driver)
+    app_for_finance_button = page_open.apply_for_finance_button_1()
+    app_for_finance_button.click()
+    time.sleep(1)
+
+    right_for_you_label = page_open.right_for_you()
+    assert right_for_you_label.is_displayed()
+    highlight(right_for_you_label)
+
+    driver.back()
+    time.sleep(1)
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
+
+    list_buttons = page_open.navi_bar_button()
+    list_buttons[0].click()
+    time.sleep(1)
+
+    invoice_financing_label = page_open.invoice_financing_label()
+    assert invoice_financing_label.is_displayed()
+    highlight(invoice_financing_label)
+
+    driver.execute_script("window.scrollBy(0, 700);")
+    time.sleep(1)
+
+    apply_finance_button = page_open.apply_for_finance_button_2()
+    apply_finance_button.click()
+    time.sleep(1)
+
+    convert_your_invoices = page_open.convert_your_invoices_label()
+    assert convert_your_invoices.is_displayed()
+    highlight(convert_your_invoices)
+
+    driver.back()
+    time.sleep(2)
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
+
+    list_buttons = page_open.navi_bar_button()
+    list_buttons[1].click()
+    time.sleep(1)
+
+    e_commerce_label = page_open.e_commerce_label()
+    assert e_commerce_label.is_displayed()
+    highlight(e_commerce_label)
+
+    driver.execute_script("window.scrollBy(0, 700);")
+    time.sleep(1)
+
+    e_commerce_button = page_open.apply_for_finance_button_2()
+    e_commerce_button.click()
+    time.sleep(1)
+
+    current_url = driver.current_url
+    expected_url = "https://rbf.stenn.com/auth/sign-up"
+    assert current_url == expected_url
+
+    driver.back()
+    time.sleep(1)
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
+
+    list_buttons = page_open.navi_bar_button()
+    list_buttons[2].click()
+    time.sleep(1)
+
+    driver.execute_script("window.scrollBy(0, 700);")
+    time.sleep(1)
+
+    saas_label = page_open.saas_label()
+    assert saas_label.is_displayed()
+    highlight(saas_label)
+
+    apply_finance_saas_button = page_open.apply_for_finance_button_2()
+    apply_finance_saas_button.click()
+    time.sleep(1)
+
+    financing_solution_label = page_open.looking_financing_label()
+    assert financing_solution_label.is_displayed()
+
+    driver.back()
+    time.sleep(2)
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
+
+    driver.execute_script("window.scrollBy(0, 1200);")
+    time.sleep(2)
+
+    articles_label = page_open.what_stenn_label()
+    assert articles_label[0].is_displayed()
+    highlight(articles_label[0])
+
+    articles_read_more_button = page_open.what_stenn_button()
+    articles_read_more_button[0].click()
+    time.sleep(1)
+
+    articles_label = home_page.articles_label()
+    assert articles_label.is_displayed()
+    highlight(articles_label)
+
+    driver.back()
+    time.sleep(2)
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
+
+    useful_guides_label = page_open.what_stenn_label()
+    assert useful_guides_label[1].is_displayed()
+    highlight(useful_guides_label[1])
+
+    driver.execute_script("window.scrollBy(0, 1800);")
+    time.sleep(2)
+    useful_guides_button = page_open.what_stenn_button()
+    useful_guides_button[1].click()
+    time.sleep(1)
+
+    useful_guides_label = page_open.useful_guides_label()
+    assert useful_guides_label.is_displayed()
+    highlight(useful_guides_label)
+
+    driver.back()
+    time.sleep(2)
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
+
+    careers_label = page_open.what_stenn_label()
+    assert careers_label[2].is_displayed()
+    highlight(careers_label[2])
+
+    careers_button = page_open.what_stenn_button()
+    careers_button[2].click()
+    time.sleep(1)
+
+    careers_label = page_open.join_stenn_label()
+    assert careers_label.is_displayed()
+    highlight(careers_label)
+
+    driver.back()
+    time.sleep(2)
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
+
+    faq_label = page_open.what_stenn_label()
+    assert faq_label[3].is_displayed()
+    highlight(faq_label[3])
+
+    faq_button = page_open.what_stenn_button()
+    faq_button[3].click()
+    time.sleep(1)
+
+    faq_label = page_open.questions_answers_label()
+    assert faq_label.is_displayed()
+    highlight(faq_label)
+
+    driver.back()
+    time.sleep(2)
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
+
+    driver.execute_script("window.scrollBy(0, 1000);")
+    time.sleep(2)
+
+    read_news_button = page_open.read_news_button()
+    read_news_button.click()
+    time.sleep(1)
+
+    articles_label = home_page.articles_label()
+    assert articles_label.is_displayed()
+    highlight(articles_label)
+
+    driver.back()
+    time.sleep(2)
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
+
+    driver.execute_script("window.scrollBy(0, 500);")
+    time.sleep(2)
+
+    apply_online_button = page_open.apply_online_button()
+    apply_online_button.click()
+    time.sleep(1)
+
+    convert_your_invoices = page_open.convert_your_invoices_label()
+    assert convert_your_invoices.is_displayed()
+    highlight(convert_your_invoices)
+
+    driver.back()
+    time.sleep(2)
+
+    accept_all_button = home_page.accept_all_pop_up_button()
+    accept_all_button.click()
+
+    products_label = home_page.your_working_label()
+    assert products_label.text == "Your working capital platform"
+    highlight(products_label)
