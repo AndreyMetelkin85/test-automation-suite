f109f0257e7939807474d5f46a33f9244bb25d4c
Andrey Metelkin <andrey.metelkin@stenn.com>
2023-07-31 19:38:36 +0300

    Added a new test.

--- UI Tests/testsuite_v3.py
+++ UI Tests/testsuite_v3.py
@@ -1,5 +1,5 @@
 import time
-
+from for_e_commerce_page import ForECommerce
 from base_page import highlight
 from for_trade_page import ForTrade
 from header_button_page import HeaderButtons
@@ -90,6 +90,100 @@ def test_for_trade_page(driver):
     time.sleep(1)
 
     driver.execute_script("window.scrollBy(0, 250);")
+    time.sleep(2)
+
+    read_news = HomePage(driver)
+    read_news_button = read_news.read_news_button()
+    read_news_button.click()
+    time.sleep(1)
+
+    page_articles_label = News(driver)
+    articles_label = page_articles_label.heading_articles_label()
+    assert articles_label.is_displayed()
+    highlight(articles_label)
+
+
+def test_e_commerce(driver):
+    home_page = HeaderButtons(driver)
+
+    solutions_button = home_page.solutions_button()
+    solutions_button.click()
+    time.sleep(1)
+
+    close_popup = home_page.accept_all_pop_up_button()
+    close_popup.click()
+
+    e_commerce = ForECommerce(driver)
+    e_commerce_button = e_commerce.ecommerce_batton()
+    e_commerce_button.click()
+
+    e_commerce_page_label = e_commerce.e_commerce_label()
+    assert e_commerce_page_label.is_displayed()
+    highlight(e_commerce_page_label)
+
+    check_my_limit_button = e_commerce.check_my_limit_button()
+    check_my_limit_button.click()
+    time.sleep(1)
+
+    current_url = driver.current_url
+    expected_url_prefix = "https://rbf.stenn.com"
+    assert current_url.startswith(expected_url_prefix)
+
+    driver.back()
+    time.sleep(1)
+
+    driver.execute_script("window.scrollBy(0, 1500);")
+    time.sleep(2)
+
+    learn_more = e_commerce.learn_more_button()
+    learn_more.click()
+    time.sleep(1)
+
+    learn_more_label = e_commerce.learn_more_label()
+    assert learn_more_label.is_displayed()
+    highlight(learn_more_label)
+
+    driver.back()
+    time.sleep(1)
+
+    driver.execute_script("window.scrollBy(0, 700);")
+    time.sleep(2)
+
+    button_counter = 5
+    for buttons in range(button_counter):
+        frequent_lyasked_questions_button = e_commerce.frequently_asked_questions()
+        frequent_lyasked_questions_button[buttons].click()
+
+        if buttons == 0:
+            what_revenue_label = e_commerce.revenue_based_financing_label()
+            assert what_revenue_label.is_displayed()
+            highlight(what_revenue_label)
+            frequent_lyasked_questions_button[buttons].click()
+        elif buttons == 1:
+            what_can_i_use_label = e_commerce.typically_our_customers_label()
+            assert what_can_i_use_label.is_displayed()
+            highlight(what_can_i_use_label)
+            frequent_lyasked_questions_button[buttons].click()
+        elif buttons == 2:
+            how_do_repayments_work = e_commerce.repayments_for_your_draw_label()
+            assert how_do_repayments_work.is_displayed()
+            highlight(how_do_repayments_work)
+            frequent_lyasked_questions_button[buttons].click()
+        elif buttons == 3:
+            what_information_label = e_commerce.in_order_to_accurately_label()
+            assert what_information_label.is_displayed()
+            highlight(what_information_label)
+            frequent_lyasked_questions_button[buttons].click()
+        elif buttons == 4:
+            who_is_stenn_label = e_commerce.sten_is_uk_label()
+            assert who_is_stenn_label.is_displayed()
+            highlight(who_is_stenn_label)
+            frequent_lyasked_questions_button[buttons].click()
+
+            driver.execute_script("window.scrollBy(0, 100);")
+            time.sleep(2)
+
+    driver.execute_script("window.scrollBy(0, 750);")
     time.sleep(2)
 
     read_news = HomePage(driver)
---
+++ for_e_commerce_page.py
@@ -0,0 +1,44 @@
+from selenium.webdriver.common.by import By
+from base_page import BasePage
+
+
+class ForECommerce(BasePage):
+
+    def ecommerce_batton(self):
+        return self.find_element((By.XPATH, '//a[@class="styles_tabsTab__2bOpf"]'))
+
+    def e_commerce_label(self):
+        return self.find_element((By.XPATH, '//div[@class="styles_blockTextWrapper__jxmTm"]'))
+
+    def check_my_limit_button(self):
+        return self.find_element((By.XPATH, '//span[text()="Check my limit"]'))
+
+    def learn_more_label(self):
+        return self.find_element((By.XPATH, '//h1[contains(., "Stenn named")]'))
+
+    def learn_more_button(self):
+        return self.find_element((By.XPATH, '//span[text()="Learn more"]'))
+
+    def frequently_asked_questions(self):
+        return self.find_elements((By.XPATH, '(//p[@class="styles_questionLinkAccordionTitle___aB8Q"])'))
+
+    def revenue_based_financing_label(self):
+        return self.find_element((By.XPATH, '//p[contains(., "Revenue based financing")]'))
+
+    def typically_our_customers_label(self):
+        return self.find_element((By.XPATH, '//p[contains(., "Typically, our customers")]'))
+
+    def repayments_for_your_draw_label(self):
+        return self.find_element((By.XPATH, '//p[contains(., "Repayments for your draw")]'))
+
+    def in_order_to_accurately_label(self):
+        return self.find_element((By.XPATH, '//p[contains(., "In order to accurately")]'))
+
+    def sten_is_uk_label(self):
+        return self.find_element((By.XPATH, '//p[contains(., "Stenn is a UK")]'))
+
+    def read_news_button(self):
+        return self.find_element((By.XPATH, '//span[text()="Read all News"]'))
+
+    def learn_more_label(self):
+        return self.find_element((By.XPATH, '//h1[contains(., "Stenn named")]'))
