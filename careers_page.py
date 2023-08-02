b8b4bf433aab6adfe6e885bd4215057c786dfc1c
Andrey Metelkin <andrey.metelkin@stenn.com>
2023-08-02 12:01:00 +0300

    Added files with classes. Described methods for searching for elements on the page.

---
+++ careers_page.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+from base_page import BasePage
+
+
+class Careers(BasePage):
+    def join_stenn_heading(self):
+        return self.find_element((By.XPATH, '//h1[text()="Join Stenn"]'))
+   
