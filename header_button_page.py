2f8d122b48b029238be10a1d7d8dc10c29da56eb
Andrey Metelkin <andrey.metelkin@stenn.com>
2023-08-09 10:42:11 +0300

    New test

--- header_button_page.py
+++ header_button_page.py
@@ -21,3 +21,15 @@ class HeaderButtons(BasePage):
 
     def accept_all_pop_up_button(self):
         return self.find_element((By.XPATH, '//*[text()="Accept All"]'))
+
+    def login_dropdown(self):
+        return self.find_element((By.XPATH, '//div[text()="Log in"]'))
+
+    def invoice_fincace_button(self):
+        return self.find_element((By.XPATH, '//*[@data-testid = "invoice-fincace"]'))
+
+    def revenue_button(self):
+        return self.find_element((By.XPATH, '//*[@data-testid = "revenue"]'))
+
+    def apply_for_finance_button(self):
+        return self.find_element((By.XPATH, '//*[@id = "portal-apply-top-right"]'))
