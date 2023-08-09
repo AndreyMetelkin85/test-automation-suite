45c2b99972a616b32bd5b0edce6834706dffaf5f
Andrey Metelkin <andrey.metelkin@stenn.com>
2023-08-09 10:42:11 +0300

    New test

---
+++ finance_option_page.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+from base_page import BasePage
+
+
+class FinanceOption(BasePage):
+    def get_heading_label(self):
+        return self.find_element((By.XPATH, '//h1[text()="Select your finance option"]'))
+
+    def international_and_domestic_label(self):
+        return self.find_element((By.XPATH, '//h1[text()="International and Domestic Invoice Financing"]'))
+
+    def revenue_based_financing_label(self):
+        return self.find_element((By.XPATH, '//h1[text()="International and Domestic Invoice Financing"]'))
+
+    def apply_for_finance_app(self):
+        return self.find_element((By.XPATH, '//*[@data-testid = "https://app.stenn.com/auth/sign-up"]'))
+
+    def apply_for_finance_rbf(self):
+        return self.find_element((By.XPATH, '//*[@data-testid = "https://rbf.stenn.com/auth/sign-up"]'))
