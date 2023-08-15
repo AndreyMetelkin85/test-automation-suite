9e147d580b2e3e9f09e4a3e66c297d2d6754f17a
Andrey Metelkin <andrey.metelkin@stenn.com>
2023-08-15 23:12:23 +0300

    New method

---
+++ conftest.py
@@ -0,0 +1,37 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from main_page import MainPage
+from api_base_page import BaseApi
+
+
+@pytest.fixture(scope="session")
+def api_client():
+    base_url = 'https://send-request.me'
+    return BaseApi(base_url)
+
+
+@pytest.fixture(scope="session")
+def driver():
+    options = Options()
+    options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=options)
+    base_page = MainPage(driver)
+    base_page.go_to_site()
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture
+def slow_scroll():
+    def scroll(driver, scroll_step=150, pause_duration=0.3):
+        current_position = driver.execute_script('return window.pageYOffset')
+        page_height = driver.execute_script('return document.body.scrollHeight')
+
+        while current_position < page_height:
+            current_position += scroll_step
+            driver.execute_script(f'window.scrollTo(0, {current_position})')
+            time.sleep(pause_duration)
+
+    return scroll
