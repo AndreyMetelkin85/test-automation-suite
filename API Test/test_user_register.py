766ae76128b9bdf88f15eb16158df13cd36185da
Andrey <80163631+AndreyMetelkin85@users.noreply.github.com>
2023-10-05 14:01:49 +0300

    new test add

--- API Test/test_user_register.py
+++ API Test/test_user_register.py
@@ -1,8 +1,28 @@
 import requests
 from lib.api_helper import ApiHelper
+from datetime import datetime
 
 
 class TestUserRegistry(ApiHelper):
+    def setup(self):
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+
+    def test_create_user_successfully(self):
+        data = {
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'password': '123',
+            'email': self.email
+        }
+
+        response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
+
+        ApiHelper.assert_code_status(response, 200)
+        ApiHelper.assert_json_has_key(response, "id")
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
@@ -15,5 +35,6 @@ class TestUserRegistry(ApiHelper):
         }
         response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
 
-        assert response.status_code == 400, f"Unexpected status code: {response.status_code}"
-        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+        ApiHelper.assert_code_status(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
