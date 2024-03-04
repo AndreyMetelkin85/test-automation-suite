897eb0b0b061c0fd4c45c3868ff1141c25045115
Andrey <80163631+AndreyMetelkin85@users.noreply.github.com>
2024-03-04 21:20:06 +0300

    Deleting files

--- configuration.py
+++
@@ -1,3 +0,0 @@
-# SERVICE_URL = 'https://gorest.co.in/public/v1/users'
-SERVICE_URL = 'https://my-json-server.typicode.com/typicode/demo/posts'
-SERVICE_URL = 'https://send-request.me/api/companies'
