e52ff0d2d73e7022fbcb6647562a4a60dd4140c1
Andrey <rainman-85@mail.ru>
2024-04-02 12:07:29 +0300

    workflow_dispatch

--- test_ui/conftest.py
+++ test_ui/conftest.py
@@ -9,40 +9,46 @@ from framework.wait_page import Wait
 from framework.page_fixture import PageFixture
 
 
-@pytest.fixture(params=["chrome", "firefox", "edge"])
-def driver(request):
+@pytest.fixture
+def driver():
     """
        Фикстура для создания веб-драйвера для различных браузеров.
        :param request: Параметр request фикстуры pytest, который содержит информацию о запрошенном параметре.
        :return: Экземпляр веб-драйвера для указанного браузера.
     """
-    if request.param == "chrome":
-        # Настройка параметров для Chrome
-        options = ChromeOptions()
-        # UI тесты запускаются в фоновом режиме.
-        options.add_argument("--headless=new")
-        # Создание экземпляра веб-драйвера Chrome
-        driver = webdriver.Chrome(options=options)
-        # Создание экземпляра веб-драйвера Firefox
-        driver.maximize_window()
-    elif request.param == "firefox":
-        # Настройка параметров для Firefox
-        options = FirefoxOptions()
-        # UI тесты запускаются в фоновом режиме.
-        options.add_argument('--headless')
-        # Создание экземпляра веб-драйвера Firefox
-        driver = webdriver.Firefox(options=options)
-        driver.maximize_window()
-    elif request.param == "edge":
-        # Настройка параметров для Edge
-        options = EdgeOptions()
-        # UI тесты запускаются в фоновом режиме.
-        options.add_argument("--headless=new")
-        # Использование Chromium для Edge (по умолчанию в новых версиях)
-        options.use_chromium = True
-        # Создание экземпляра веб-драйвера Edge
-        driver = webdriver.Edge(options=options)
-        driver.maximize_window()
+    options = ChromeOptions()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+
+    # TODO Пока нет нужды проводить кросбраузерное тестирование!!!!!!
+    # if request.param == "chrome":
+    #     # Настройка параметров для Chrome
+    #     options = ChromeOptions()
+    #     # UI тесты запускаются в фоновом режиме.
+    #     options.add_argument("--headless=new")
+    #     # Создание экземпляра веб-драйвера Chrome
+    #     driver = webdriver.Chrome(options=options)
+    #     # Создание экземпляра веб-драйвера Firefox
+    #     driver.maximize_window()
+    # elif request.param == "firefox":
+    #     # Настройка параметров для Firefox
+    #     options = FirefoxOptions()
+    #     # UI тесты запускаются в фоновом режиме.
+    #     options.add_argument('--headless')
+    #     # Создание экземпляра веб-драйвера Firefox
+    #     driver = webdriver.Firefox(options=options)
+    #     driver.maximize_window()
+    # elif request.param == "edge":
+    #     # Настройка параметров для Edge
+    #     options = EdgeOptions()
+    #     # UI тесты запускаются в фоновом режиме.
+    #     options.add_argument("--headless=new")
+    #     # Использование Chromium для Edge (по умолчанию в новых версиях)
+    #     options.use_chromium = True
+    #     # Создание экземпляра веб-драйвера Edge
+    #     driver = webdriver.Edge(options=options)
+    #     driver.maximize_window()
 
     # Установка неявного ожидания в 5 секунд
     driver.implicitly_wait(5)
