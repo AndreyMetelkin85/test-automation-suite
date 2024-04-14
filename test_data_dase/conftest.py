922f914bab69979d6711270059b2728e61d2d123
Andrey <rainman-85@mail.ru>
2024-04-14 21:36:26 +0300

    Fix

--- test_data_dase/conftest.py
+++ test_data_dase/conftest.py
@@ -2,18 +2,20 @@ import os
 
 import psycopg2
 import pytest
+from dotenv import load_dotenv
 
 
 @pytest.fixture
 def db_connection():
     connection = None
+    load_dotenv()
     try:
         db_config = {
-            "dbname": os.environ["dbname"],
-            "user": os.environ["user"],
-            "password": os.environ["password"],
-            "host": os.environ["host"],
-            "port": os.environ["port"]
+            "dbname": os.environ["DBNAME"],
+            "user": os.environ["USER"],
+            "password": os.environ["PASSWORD"],
+            "host": os.environ["HOST"],
+            "port": os.environ["PORT"]
         }
         connection = psycopg2.connect(**db_config)
         # connection = psycopg2.connect(**DB_CONFIG)
