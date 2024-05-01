3f02110a989befebcde0c4088c7210f8ff660e23
Andrey <rainman-85@mail.ru>
2024-05-01 20:48:48 +0300

    new test, and add new selector

--- framework/toils_qa_page/elements_page.py
+++ framework/toils_qa_page/elements_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from base_page import BasePage
 
 
-class TextBox(BasePage):
+class ElementsPage(BasePage):
 
     def text_box_label(self):
         return self.find_element((By.XPATH, '//h1[@class="text-center" and text()="Text Box"]'))
@@ -25,3 +25,22 @@ class TextBox(BasePage):
 
     def output_field(self):
         return self.find_elements((By.XPATH, '//div[@id="output"]//descendant::p'))
+
+    def check_box_label(self):
+        return self.find_element((By.XPATH, '//h1[@class="text-center" and text()="Check Box"]'))
+
+    def expand_all_button(self):
+        return self.find_element((By.XPATH, '//button[@class="rct-option rct-option-expand-all" and @type="button"]'))
+
+    def collapse_all_button(self):
+        return self.find_element((By.XPATH, '//button[@class="rct-option rct-option-collapse-all" and @type="button"]'))
+
+    def home_title(self):
+        return self.find_element((By.XPATH, '//span[@class="rct-title" and text()="Home"]'))
+
+    def you_have_selected_results(self):
+        return self.find_element((By.XPATH, '//div[@id="result"]'))
+
+    def dropdown_arrow_home_button(self):
+        return self.find_element((By.XPATH, '//div[@id="tree-node"]/descendant::button'))
+
--- test_ui/demo_qa_ui/tests_element_check.py
+++ test_ui/demo_qa_ui/tests_element_check.py
@@ -14,28 +14,63 @@ def test_element_text_box(driver, page_fixture, text_box_form_data):
     left_panel_tex_box_buttons = page_fixture.demo_qa_home_page.left_panel_buttons()
     left_panel_tex_box_buttons[0].click()
 
-    text_box_label = page_fixture.text_box_page.text_box_label()
+    text_box_label = page_fixture.elements_page.text_box_label()
     text_box_label.is_displayed()
 
-    text_box_form_full_name = page_fixture.text_box_page.user_name_input()
+    text_box_form_full_name = page_fixture.elements_page.user_name_input()
     text_box_form_full_name.send_keys(text_box_form_data["fullname"])
 
-    text_box_form_email = page_fixture.text_box_page.user_email_input()
+    text_box_form_email = page_fixture.elements_page.user_email_input()
     text_box_form_email.send_keys(text_box_form_data["email"])
 
-    text_box_form_current_address = page_fixture.text_box_page.current_address_input()
+    text_box_form_current_address = page_fixture.elements_page.current_address_input()
     text_box_form_current_address.send_keys(text_box_form_data["current address"])
 
-    text_box_form_permanent_address = page_fixture.text_box_page.permanent_address_input()
+    text_box_form_permanent_address = page_fixture.elements_page.permanent_address_input()
     text_box_form_permanent_address.send_keys(text_box_form_data["permanent address"])
 
-    text_box_submit_button = page_fixture.text_box_page.submit_button()
+    text_box_submit_button = page_fixture.elements_page.submit_button()
     text_box_submit_button.click()
 
-    result_output = page_fixture.text_box_page.output_field()
+    result_output = page_fixture.elements_page.output_field()
     output_text = [element.text for element in result_output]
 
     for key, value in text_box_form_data.items():
         value_cleaned = value.replace('\n', ' ').strip()
         assert any(
-            value_cleaned in text for text in output_text), f"[INFO!!!] Ошибка значения - '{value}' не найдено!!!"
+            value_cleaned in text for text in output_text), f"[INFO!!!] Ошибка, значение - '{value}' не найдено!!!"
+
+
+def test_check_box_expand_and_close_folders(driver, page_fixture):
+    page_fixture.go_to_web_site_demo_qa.go_to_web_site_demo_qa()
+
+    elements_button = page_fixture.demo_qa_home_page.category_cards_home_page()
+    elements_button[0].click()
+
+    elements_button_check_box = page_fixture.demo_qa_home_page.left_panel_buttons()
+    elements_button_check_box[1].click()
+
+    check_box_expand_button = page_fixture.elements_page.expand_all_button()
+    check_box_expand_button.click()
+
+    check_box_collapse_all_button = page_fixture.elements_page.collapse_all_button()
+    check_box_collapse_all_button.click()
+
+
+def test_check_box_home_click(driver, page_fixture):
+    page_fixture.go_to_web_site_demo_qa.go_to_web_site_demo_qa()
+
+    elements_button = page_fixture.demo_qa_home_page.category_cards_home_page()
+    elements_button[0].click()
+
+    elements_button_check_box = page_fixture.demo_qa_home_page.left_panel_buttons()
+    elements_button_check_box[1].click()
+
+    home_dropdown_arrow = page_fixture.elements_page.dropdown_arrow_home_button()
+    home_dropdown_arrow.click()
+
+    home_title = page_fixture.elements_page.home_title()
+    home_title.click()
+
+    chose_result = page_fixture.elements_page.you_have_selected_results()
+    chose_result.is_displayed()
