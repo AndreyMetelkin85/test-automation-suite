75d3817bdded08c4fcfc56eeb21a12c982046572
Andrey <rainman-85@mail.ru>
2024-10-13 16:36:19 +0300

    Fixed the tests after rewriting the class date

--- test_api/test_api_order.py
+++ test_api/test_api_order.py
@@ -1,16 +1,13 @@
 import pytest
-
-from conftest import pet_test_data, order_test_data, pet_store_pet_fixture, order_store_pet_fixture
+from conftest import pet_store_pet_fixture, order_store_pet_fixture, test_data
 
 
 # Тест создания заказа на питомца.
 @pytest.mark.parametrize("status", ["placed", "approved", "delivered"])
-def test_create_order_for_pet(pet_test_data, order_test_data, pet_store_pet_fixture, order_store_pet_fixture, status):
-    pet_test_data = pet_test_data.pet_test_data()
-    order_test_data = order_test_data.order_test_data()
-    pet_store_pet_fixture.add_new_pet_store(pet_test_data=pet_test_data, status=status)
-    create_pet_order = order_store_pet_fixture.create_order_for_pet(
-        order_test_data=order_test_data, pet_test_data=pet_test_data, status=status)
+def test_create_order_for_pet(pet_store_pet_fixture, order_store_pet_fixture, test_data, status):
+    generated_data = test_data.generate_test_data()
+    pet_store_pet_fixture.add_new_pet_store(test_data=generated_data, status=status)
+    create_pet_order = order_store_pet_fixture.create_order_for_pet(test_data=generated_data, status=status)
     assert create_pet_order["status"] == status
 
 
@@ -29,11 +26,9 @@ def test_inventories_by_status(order_store_pet_fixture):
 
 
 # Тест удаления заказа.
-def test_delete_order(pet_test_data, order_test_data, pet_store_pet_fixture, order_store_pet_fixture):
-    pet_test_data = pet_test_data.pet_test_data()
-    order_test_data = order_test_data.order_test_data()
-    pet_store_pet_fixture.add_new_pet_store(pet_test_data=pet_test_data, status="approved")
-    order_store_pet_fixture.create_order_for_pet(
-        order_test_data=order_test_data, pet_test_data=pet_test_data, status="approved")
-    order_delete = order_store_pet_fixture.order_delete(orderId=order_test_data["id"])
+def test_delete_order(test_data, pet_store_pet_fixture, order_store_pet_fixture):
+    pet_test_data = test_data.generate_test_data()
+    pet_store_pet_fixture.add_new_pet_store(test_data=pet_test_data, status="approved")
+    order_store_pet_fixture.create_order_for_pet(test_data=pet_test_data, status="approved")
+    order_delete = order_store_pet_fixture.order_delete(orderId=pet_test_data.order_data.id)
     assert order_delete["code"] == 200
--- test_data/default_user_data.py
+++ test_data/default_user_data.py
@@ -4,66 +4,64 @@ from typing import List, Optional
 
 @dataclass
 class User:
-    id: Optional[int] = None,
-    username: Optional[str] = None,
-    firstName: Optional[str] = None,
-    lastName: Optional[str] = None,
-    email: Optional[str] = None,
-    password: Optional[str] = None,
-    phone: Optional[int] = None,
+    id: Optional[int] = None
+    username: Optional[str] = None
+    firstName: Optional[str] = None
+    lastName: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
+    phone: Optional[int] = None
     userStatus: Optional[int] = None
 
 
 @dataclass
 class Category:
-    id: Optional[int] = None,
+    id: Optional[int] = None
     name: Optional[str] = None
 
 
 @dataclass
 class Tags:
-    id: Optional[int] = None,
+    id: Optional[int] = None
     name: Optional[str] = None
 
 
 @dataclass
 class PetData:
-    category: Category
     id: Optional[int] = None
-    name: Optional[str] = None,
-    photoUrls: List[str] = None,
-    tags: List[Tags] = None
+    name: Optional[str] = None
+    photoUrls: List[str] = None
 
 
 @dataclass
 class OrderData:
-    id: Optional[int] = None,
-    quantity: Optional[int] = None,
-    shipDate: Optional[int] = None
+    id: Optional[int] = None
+    quantity: Optional[int] = None
+    shipDate: Optional[str] = None
 
 
 @dataclass
 class TextBoxFormData:
-    fullname: Optional[str] = None,
-    email: Optional[str] = None,
-    current_address: Optional[str] = None,
+    fullname: Optional[str] = None
+    email: Optional[str] = None
+    current_address: Optional[str] = None
     permanent_address: Optional[str] = None
 
 
 @dataclass
 class RegistrationFormData:
-    first_name: Optional[str] = None,
-    last_name: Optional[str] = None,
-    email: Optional[str] = None,
-    age: Optional[int] = None,
-    salary: Optional[int] = None,
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[str] = None
+    age: Optional[int] = None
+    salary: Optional[int] = None
     department: Optional[str] = None
 
 
 @dataclass
 class StudentRegistrationData:
-    first_name: Optional[str] = None,
-    last_name: Optional[str] = None,
-    email: Optional[str] = None,
-    mobile_number: Optional[int] = None,
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[str] = None
+    mobile_number: Optional[int] = None
     current_address: Optional[str] = None
