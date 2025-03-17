ca26a440398b68cd5b888adf10c97add42c9e0f5
Andrey <rainman1811@gmail.com>
2025-03-17 20:38:07 +0300

    fix test and data generator

--- qa_test_data/test_data.py
+++ qa_test_data/test_data.py
@@ -29,12 +29,12 @@ class DataGenerator:
             тестовых данных на английском языке.
         """
         self.faker = faker.Faker(locale="en_US")
-        self.user = User()
-        self.category = Category()
-        self.tags = Tags()
-        self.pet_data = PetData()
-        self.order_data = OrderData()
-        self.box_form_data = TextBoxFormData()
+        # self.user = User()
+        # self.category = Category()
+        # self.tags = Tags()
+        # self.pet_data = PetData()
+        # self.order_data = OrderData()
+        # self.box_form_data = TextBoxFormData()
         # self.registration_form_data = RegistrationFormData()
         # self.student_registration_form_data = StudentRegistrationData()
 
@@ -49,14 +49,18 @@ class DataGenerator:
         password = self.faker.password()
         phone = self.faker.numerify("+1 (###) ###-####")
         user_status = random.randint(1, 10)
-        self.user.id = id
-        self.user.username = username
-        self.user.firstName = first_name
-        self.user.lastName = last_name
-        self.user.email = email
-        self.user.password = password
-        self.user.phone = phone
-        self.user.userStatus = user_status
+
+        user = User()
+        user.id = id
+        user.username = username
+        user.firstName = first_name
+        user.lastName = last_name
+        user.email = email
+        user.password = password
+        user.phone = phone
+        user.userStatus = user_status
+
+        return user
 
     def create_pet_data(self):
         """ Генерирует данные для животного. """
@@ -64,21 +68,33 @@ class DataGenerator:
         id = random.randint(1, 9999)
         name = self.faker.word()
         photo_urls = self.faker.image_url()
-        self.pet_data.id = id
-        self.pet_data.name = name
-        self.pet_data.photoUrls = photo_urls
+
+        pet_data = PetData()
+        pet_data.id = id
+        pet_data.name = name
+        pet_data.photoUrls = photo_urls
+
+        return pet_data
 
     def create_tags_data(self):
         id = random.randint(1, 9999)
         name = self.faker.word()
-        self.tags.id = id
-        self.tags.name = name
+
+        tags = Tags()
+        tags.id = id
+        tags.name = name
+
+        return tags
 
     def create_category_data(self):
         id = random.randint(1, 9999)
         name = self.faker.word()
-        self.category.id = id
-        self.category.name = name
+
+        category = Category()
+        category.id = id
+        category.name = name
+
+        return category
 
     def create_order_test_data(self):
         """ Генерирует тестовые данные для заказа. """
@@ -87,9 +103,13 @@ class DataGenerator:
         id = random.randint(1, 9999)
         quantity = random.randint(1, 10)
         ship_date = current_datetime
-        self.order_data.id = id
-        self.order_data.quantity = quantity
-        self.order_data.shipDate = ship_date
+
+        order_data = OrderData()
+        order_data.id = id
+        order_data.quantity = quantity
+        order_data.shipDate = ship_date
+
+        return order_data
 
     def db_test_data(self):
         """
@@ -112,10 +132,14 @@ class DataGenerator:
         email = self.faker.email(domain="google.com")
         current_address = self.faker.address()
         permanent_address = self.faker.address()
-        self.box_form_data.fullname = fullname
-        self.box_form_data.email = email
-        self.box_form_data.current_address = current_address
-        self.box_form_data.permanent_address = permanent_address
+
+        box_form_data = TextBoxFormData()
+        box_form_data.fullname = fullname
+        box_form_data.email = email
+        box_form_data.current_address = current_address
+        box_form_data.permanent_address = permanent_address
+
+        return box_form_data
 
     def registration_form_data(self):
         """
@@ -144,11 +168,16 @@ class DataGenerator:
         return student_form
 
     def generate_test_data(self):
-        self.create_user_data()
-        self.create_pet_data()
-        self.create_tags_data()
-        self.create_category_data()
-        self.create_order_test_data()
-        self.create_text_box_form_data()
-        return TestData(user=self.user, pet_data=self.pet_data, category=self.category, tags=self.tags,
-                        order_data=self.order_data, text_box_form_data=self.box_form_data)
+        user = self.create_user_data()
+        pet_data = self.create_pet_data()
+        tags = self.create_tags_data()
+        category = self.create_category_data()
+        order_data = self.create_order_test_data()
+        box_form_data = self.create_text_box_form_data()
+        return TestData(user=user,
+                        pet_data=pet_data,
+                        category=category,
+                        tags=tags,
+                        order_data=order_data,
+                        text_box_form_data=box_form_data
+                        )
