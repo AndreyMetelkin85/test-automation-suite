4722ad0b2ca03e96876d9a207b4e0267e641e59d
Andrey <rainman1811@gmail.com>
2025-03-18 10:19:55 +0300

    add description test

--- test_api/test_api_order.py
+++ test_api/test_api_order.py
@@ -7,7 +7,6 @@ from conftest import pet_store_pet_fixture, order_store_pet_fixture, test_data
 @allure.title("Тест создания заказа на питомца")
 @pytest.mark.parametrize("status", ["placed", "approved", "delivered"])
 def test_create_order_for_pet(pet_store_pet_fixture, order_store_pet_fixture, test_data, status):
-
     with step("Генерируем данные"):
         generated_data = test_data.generate_test_data()
 
@@ -21,7 +20,6 @@ def test_create_order_for_pet(pet_store_pet_fixture, order_store_pet_fixture, te
 
 @allure.title("Тест поиска заказа по его идентификатору")
 def test_find_purchase_order_id(order_store_pet_fixture, pet_store_pet_fixture, test_data):
-
     with step("Генерируем данные"):
         generated_data = test_data.generate_test_data()
 
@@ -37,16 +35,23 @@ def test_find_purchase_order_id(order_store_pet_fixture, pet_store_pet_fixture, 
         assert get_order_data[0]["petId"] == generated_data.pet_data.id
 
 
-#  Тест получения остатков по статусу.
+@allure.title("Тест получения остатков по статусу")
 def test_inventories_by_status(order_store_pet_fixture):
     response_status_code = order_store_pet_fixture.get_inventories_by_status()
     assert response_status_code[1] == 200
 
 
-# Тест удаления заказа.
+@allure.title("Тест удаления заказа")
 def test_delete_order(test_data, pet_store_pet_fixture, order_store_pet_fixture):
-    pet_test_data = test_data.generate_test_data()
-    pet_store_pet_fixture.add_new_pet_store(test_data=pet_test_data, status="approved")
-    order_store_pet_fixture.create_order_for_pet(test_data=pet_test_data, status="approved")
-    order_delete = order_store_pet_fixture.order_delete(orderId=pet_test_data.order_data.id)
-    assert order_delete == 200
+    with step("Генерируем данные"):
+        pet_test_data = test_data.generate_test_data()
+
+    with step("Добавляем питомца и создаём заказ"):
+        pet_store_pet_fixture.add_new_pet_store(test_data=pet_test_data, status="approved")
+        order_store_pet_fixture.create_order_for_pet(test_data=pet_test_data, status="approved")
+
+    with step("Удаляем заказ"):
+        order_delete = order_store_pet_fixture.order_delete(orderId=pet_test_data.order_data.id)
+
+    with step("Проверяем статусы"):
+        assert order_delete == 200
